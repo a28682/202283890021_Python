ba4c5bb37bc4e9af41eeba4bc9386923707de1f4
alias.co <2867218760@qq.com>
2025-03-19 09:07:06 +0800

    Updated AddTwoNums.py Version 2

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,8 +1,12 @@
 #Add Two Numbers in Python
 #Auther:WangQi
-#Uaing the + Operator
-a=15
-b=12
-#Adding two numbers
-res=a+b
+#Using a function
+ 
+#taking user input
+a=input("First Number: ")
+b=input("Second Number: ")
+
+
+#converting input to float and adding
+res=float(a)+float(b)
 print(res)
